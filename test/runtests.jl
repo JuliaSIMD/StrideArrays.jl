3e8dc71d85c4c6df40ea53fff521151c373d16dd
chriselrod <elrodc@gmail.com>
2022-06-01 23:30:09 -0400

    require StrideArraysCore 0.3.9 and don't check deps_compat on Julia dev

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,9 @@ InteractiveUtils.versioninfo(stdout; verbose = true)
 const START_TIME = time()
 
 @time @testset "StrideArrays.jl" begin
-  @time Aqua.test_all(StrideArrays, ambiguities = false, project_toml_formatting = false)
+  @test isempty(Test.detect_unbound_args(StrideArrays))
+   
+  @time Aqua.test_all(StrideArrays, ambiguities = false, project_toml_formatting = false, deps_compat = (VERSION <= v"1.8" || VERSION.prerelease[1] != "DEV"))
   # Currently, there is one method ambiguity:
   # - map(f::F, A::AbstractStrideArray, args::Vararg{Any, K}) where {F, K} in StrideArrays at StrideArrays/src/miscellaneous.jl:22
   # - map(f, a1::AbstractArray, a2::StaticArrays.StaticArray, as::AbstractArray...) in StaticArrays at StaticArrays/0bweZ/src/mapreduce.jl:33
