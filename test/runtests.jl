201273bb2636395504d87a590fa991e789f74684
Hendrik Ranocha <mail@ranocha.de>
2023-03-12 12:47:45 +0100

    disable piracy tests of Aqua.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,7 +14,8 @@ const START_TIME = time()
     StrideArrays,
     ambiguities = false,
     project_toml_formatting = false,
-    deps_compat = VERSION <= v"1.8" || isempty(VERSION.prerelease)
+    deps_compat = VERSION <= v"1.8" || isempty(VERSION.prerelease),
+    piracy = false
   )
   # Currently, there are five method ambiguities:
   # (rand!(A::AbstractStrideArray, args::Vararg{Any, K}) where K in StrideArrays at StrideArrays/src/rand.jl:3, rand!(f::F, rng::VectorizedRNG.AbstractVRNG, x::AbstractArray{T}, α::Number, β, γ) where {T<:Union{Float32, Float64}, F} in VectorizedRNG at VectorizedRNG/L3orR/src/api.jl:242)
